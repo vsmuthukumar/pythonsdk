395cb060f24177750db59e6a900122105de36aab
vsmuthukumar <vsmuthukumar@gmail.com>
2024-04-04 17:02:45 +0530

    df

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages,setup
 setup (
     name='mypythonlib',
     packages=find_packages(include=['mypythonlib']),
-    version='0.1.1',
+    version='0.1.2',
     description='test python library',
     install_requires=[]
 )
