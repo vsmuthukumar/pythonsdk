b38233a69b76602c4f974619d6e15f2d1dcd7c31
vsmuthukumar <vsmuthukumar@gmail.com>
2024-04-04 16:45:04 +0530

    sec comm

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages,setup
 setup (
     name='mypythonlib',
     packages=find_packages(include=['mypythonlib']),
-    version='0.1.0',
+    version='0.1.1',
     description='test python library',
     install_requires=[]
 )
