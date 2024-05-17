0bc9b9087d2115d0c11e36cd46ba721268f1677d
vsmuthukumar <41564590+vsmuthukumar@users.noreply.github.com>
2024-05-17 08:35:46 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import find_packages,setup
+version = $(minor).$(patch).$(Rev:r)
 
 setup (
     name='mypythonlib',
     packages=find_packages(include=['mypythonlib']),
-    version='0.1.3',
+    version=version,
     description='test python library',
     install_requires=[]
 )
